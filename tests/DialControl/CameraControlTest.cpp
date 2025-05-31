62de072041960c1e0dabc0d722b8a4c6e2ee4948
Mark Oates <markoates0@gmail.com>
2025-05-31 00:23:05 -0400

    * Extreemely cool camera control system

--- tests/DialControl/CameraControlTest.cpp
+++ tests/DialControl/CameraControlTest.cpp
@@ -25,15 +25,29 @@ TEST_F(DialControl_CameraControlTest_WithInteractionFixture, INTERACTIVE__will_w
 
    AllegroFlare::Camera2D hud_camera;
 
-   AllegroFlare::Camera3D camera;
-   camera.stepout = { 0, 0, 16 };
-   camera.zoom = 1.0; //1.62;
-   camera.tilt = 0.0; //ALLEGRO_PI * 0.25;
-   camera.spin = 0.125;
-   camera.position = { 0, 0, 0 };
+   std::vector<AllegroFlare::Camera3D> cameras;
+   //std::map<std::string, AllegroFlare::Camera3D> cameras;
+
+   AllegroFlare::Camera3D baseline_camera;
+   baseline_camera.stepout = { 0, 0, 16 };
+   baseline_camera.zoom = 1.0; //1.62;
+   baseline_camera.tilt = 0.0; //ALLEGRO_PI * 0.25;
+   baseline_camera.spin = 0.125;
+   baseline_camera.position = { 0, 0, 0 };
+
+   AllegroFlare::Camera3D *current_camera = nullptr;
+   AllegroFlare::Camera3D live_camera = baseline_camera;
+
+   cameras.resize(6);
+   int current_camera_idx = 0;
+   for (int i=0; i<cameras.size(); i++)
+   {
+      cameras[i] = baseline_camera;
+   }
+   current_camera = &cameras[current_camera_idx];
 
    DialControl::CameraControl camera_control;
-   camera_control.set_camera(&camera);
+   camera_control.set_camera(current_camera);
 
    while(interactive_test_wait_for_event())
    {
@@ -46,7 +60,9 @@ TEST_F(DialControl_CameraControlTest_WithInteractionFixture, INTERACTIVE__will_w
             clear();
 
             // Draw the 3D scene
-            camera.setup_projection_on(al_get_target_bitmap());
+            live_camera.blend(current_camera, 0.1);
+
+            live_camera.setup_projection_on(al_get_target_bitmap());
             model->draw();
 
             // Draw the HUD
@@ -58,7 +74,31 @@ TEST_F(DialControl_CameraControlTest_WithInteractionFixture, INTERACTIVE__will_w
 
          case ALLEGRO_EVENT_KEY_DOWN:
          {
-            camera_control.on_key_down(&current_event);
+            switch(current_event.keyboard.keycode)
+            {
+               case ALLEGRO_KEY_N:
+               {
+                  // Next camera
+                  int total_cameras = cameras.size();
+                  current_camera_idx = (current_camera_idx + 1) % total_cameras;
+                  current_camera = &cameras[current_camera_idx];
+                  camera_control.set_camera(current_camera);
+               } break;
+               case ALLEGRO_KEY_P:
+               {
+                  // Previous camera
+                  int total_cameras = cameras.size();
+                  current_camera_idx = (current_camera_idx - 1 + total_cameras) % total_cameras;
+                  current_camera = &cameras[current_camera_idx];
+                  camera_control.set_camera(current_camera);
+               } break;
+               default:
+               {
+                  camera_control.on_key_down(&current_event);
+               } break;
+            }
+            //else
+            //camera_control.on_key_down(&current_event);
             //bool shift = current_event.keyboard.modifiers & ALLEGRO_KEYMOD_SHIFT;
             //switch(current_event.keyboard.keycode)
             //{
